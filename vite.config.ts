3561594090fe3bbe150d783c7d8cdc49b612d05c
Montana <a.presovsky@list.ru>
2025-11-28 12:41:00 +0300

    trying to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/alfa-ecosystem-task/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
